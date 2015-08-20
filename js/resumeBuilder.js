e185361e88ed5882abeec5e77913e4605a1bc5ea
arosa81 <alex.j.rosa@gmail.com>
2015-08-20 14:03:04 -0600

    Lesson 2 completed finalized

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,7 @@
-// var name = "Alex Rosa";
-// var role = "Web Front End Ninja";
-// var formattedName = HTMLheaderName.replace("%data%", name);
-// var formattedRole = HTMLheaderRole.replace("%data%", role);
-//
-// $("#header").prepend(formattedName + "<br>" + formattedRole);
-
 var skills = ["awesomeness", "HTML", "CSS", "Javascript"];
 
 var bio = {
-  "name": "Alex Rosa",
+  "bioName": "Alex Rosa",
   "role": "Web Front End Ninja",
   "contacts": {
     "mobile": "403.606.0637",
@@ -17,13 +10,13 @@ var bio = {
     "github": "<a href=\"https://github.com/arosa81\"  target=\"_blank\">arosa81<\/a>",
     "location": "Calgary"
   },
-  "picURL": "<img src=\"./images/fry.jpg' alt='bio picture\" alt=\"Bio Pic\">",
+  "picURL": "./images/Alex-Thumb.jpg\" alt=\"Bio Pic",
   "welcomeMessage": "This is my bio - enjoy! <br>",
   "skills": skills
 };
 
 bio.displayBio = function() {
-  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedName = HTMLheaderName.replace("%data%", bio.bioName);
   var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
   $("#header").prepend(formattedName + " " + formattedRole);
   var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
@@ -32,16 +25,19 @@ bio.displayBio = function() {
   var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
   var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
   $("#topContacts").append(formattedMobile + formattedEmail + formattedTwitter + formattedGitHub + formattedLocation);
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.picURL);
+  var formattedBioMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+  $("#header").append(formattedBioPic + "<br>" + formattedBioMessage);
+
+  if (skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    for (var i = 0; i < skills.length; i++) {
+      $("#header").append(HTMLskills.replace("%data%", skills[i]));
+    }
+  }
 }
 
 bio.displayBio();
-
-if (skills.length > 0) {
-  $("#header").append(HTMLskillsStart);
-  for (var i = 0; i < skills.length; i++) {
-    $("#header").append(HTMLskills.replace("%data%", skills[i]));
-  }
-}
 
 var work = {
   "jobs": [
@@ -55,7 +51,7 @@ var work = {
     {
       "employer": "Ideaca",
       "title": "SharePoint Business Analyst",
-      "location": "Calgary, AB, Canada",
+      "location": "Edmonton, AB, Canada",
       "dates": "2010-2012",
       "description": "SharePoint business analyst"
     }
@@ -118,30 +114,45 @@ var education = {
   "schools": [
     {
       "name": "Udacity",
-      "city": "Calgary",
+      "degree": "Nano Degree",
+      "location": "2465 Latham Street 3rd Floor, Mountain View, CA",
       "major": "Front End Web Developer",
       "gradYear": 2015,
       "courseInfo": "Online course to front end web development"
     },
     {
       "name": "SAIT",
-      "city": "Calgary",
-      "major": "Information Systems Design",
+      "degree": "Bachelors of Applied Information Technology",
+      "location": "Calgary, AB, Canada",
+      "major": "Information Systems Development",
       "gradYear": 2010,
       "courseInfo": "Information systems architectural analysis"
     }
   ]
 };
 
-// $("#main").append(education.name);
-// $("#main").append(education.yearsSchoolAttended);
-// $("#main").append(education.schoolCity);
+education.displayEducation = function () {
+  for (var school in education.schools) {
+    if (education.schools.hasOwnProperty(school))
+    {
+      $("#education").append(HTMLschoolStart);
+      var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+      var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+      var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+      var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+      var formattedSchoolDate = HTMLschoolDates.replace("%data%", education.schools[school].gradYear);
+      $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree + formattedSchoolDate + formattedSchoolLocation +     formattedSchoolMajor);
+    }
+  }
+};
 
-function inName(name) {
-  name = name.trim().split(" ");
-  name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
-  name[1] = name[1].toUpperCase();
-  var fullName = name.join(" ");
+education.displayEducation();
+
+function inName(bioName) {
+  bioName = bioName.trim().split(" ");
+  bioName[0] = bioName[0].slice(0, 1).toUpperCase() + bioName[0].slice(1).toLowerCase();
+  bioName[1] = bioName[1].toUpperCase();
+  var fullName = bioName.join(" ");
   return fullName;
 }
 
